e83696d31acaf76ec91dfb8372bacfc030cc5c52
Bradley Mutima <98081808+poky789456@users.noreply.github.com>
2025-02-05 11:22:24 +0100

    bulk data creation json

---
+++ elasticsearch/json/insert_data.sh
@@ -0,0 +1,8 @@
+curl -s -H "Content-Type: application/x-ndjson" -XPOST localhost:9200/receipe/_bulk --data-binary "@receipe.json" &&\
+printf "\n✅ Insertion receipe index to elastic node OK ✅ "
+
+curl -s -H "Content-Type: application/x-ndjson" -XPOST localhost:9200/accounts/docs/_bulk --data-binary "@accounts.json"
+printf "\n✅ Insertion accounts index to elastic node OK ✅ "
+
+curl -s -H "Content-Type: application/x-ndjson" -XPOST localhost:9200/products/_bulk --data-binary "@products.json"
+printf "\n✅ Insertion products index to elastic node OK ✅ "
